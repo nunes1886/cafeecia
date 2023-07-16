e30dad34ebcbf8cdf4b6371a8dbfee4e64e90f82
Gledson Nunes <nuness.gledson@gmail.com>
2023-07-16 11:27:02 -0300

    Atualização de Layout para Cafeteria

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+const name = document.querySelector(".name");
+
+// function testando(color) {
+//     name.style.color = color;
+// }
+
+btn.addEventListener("click", testando);
+function testando(color) {
+    name.style.color = color
+}
+
